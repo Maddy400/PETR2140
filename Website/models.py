eb142fb3cbed1a6838d452d06b114ca3720e9c41
Maddy400 <40018295@petroc.ac.uk>
2026-01-21 19:23:41 +0000

    db tables started

---
+++ Website/models.py
@@ -0,0 +1,34 @@
+from . import db
+from flask_login import UserMixin
+
+
+
+class User(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+
+class Tutor(db.Model, UserMixin):
+    tutor_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+
+class Admin(db.Model, UserMixin):
+    admin_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(150))
+
+class Bookings(db.Model):
+    booking_id = db.Column(db.Integer, primary_key=True)
+    tutor_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+
+class Resources(db.Model):
+    resource_id = db.Column(db.Integer, primary_key=True)
+    resource_url = db.Column(db.String)
