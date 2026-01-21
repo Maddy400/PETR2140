29660286eb69034f451db6206a9cde9474834461
Maddy400 <40018295@petroc.ac.uk>
2026-01-21 17:05:06 +0000

    Allow retrieval of data from log in and begin creating the database

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "academic_wizard"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'PETR2140'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
 
     from .root import views
--- Website/auth.py
+++ Website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,12 @@ def login():
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template("sign_up.html")
